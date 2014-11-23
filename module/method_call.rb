26600dffdcd26da81fb98c9702059a3cd74ff4df
arthurbryant <cst.feng@gmail.com>
2014-11-23 16:56:45 +0900

    update test module

--- module/method_call.rb
+++ module/method_call.rb
@@ -20,7 +20,24 @@ class User < Person
   include TestM
 end
 
-#TestM.print
 # The method from include will be called before parent class
 user = User.new
 user.eat
+
+module TestModule
+  def sin
+    puts "call method sin of TestModule"
+  end
+end
+
+include TestModule
+
+module TestModuleFunction
+  def sin
+    puts "call method sin of TestModuleFunction"
+  end
+  module_function :sin
+end
+
+TestModuleFunction.sin
+include TestModuleFunction
