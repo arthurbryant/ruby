5ae4ef1bc793eb8ccd392466dea92cb861359c11
arthurbryant <cst.feng@gmail.com>
2015-04-01 19:54:34 +0900

    update

--- exception/raise_error.rb
+++ exception/raise_error.rb
@@ -8,8 +8,33 @@ begin
   upload
 rescue Exception => e
   STDERR.puts e.message
-  STDERR.puts $!
   STDERR.puts e.backtrace
-  STDERR.puts $@
 end
 
+def new_exception_1
+  begin
+    raise StandardError.new("start new exception")
+  rescue StandardError => e
+    p e.inspect
+  end
+end
+
+def new_exception_2
+  begin
+    raise StandardError, "start new exception"
+  rescue StandardError => e
+    p e.inspect
+  end
+end
+
+def new_exception_3
+  begin
+    raise "start new exception"
+  rescue StandardError => e
+    p e.inspect
+  end
+end
+
+new_exception_1
+new_exception_2
+new_exception_3
